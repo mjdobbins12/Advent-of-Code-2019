556faacc43d0800752796aecddc0a0d0ba33b78b
Mark Dobbins <mjdobbins12@gmail.com>
2020-04-30 16:39:16 +0100

    day 1 step 2 complete

--- lib/day1.rb
+++ lib/day1.rb
@@ -1,5 +1,11 @@
 module FuelCounter
     def self.get_total(mass)
-        mass / 3 - 2
+        fuel = 0
+        mass_to_add = (mass / 3 - 2)
+        while mass_to_add > 0 do
+            fuel += mass_to_add
+            mass_to_add = (mass_to_add / 3 - 2)
+        end
+        fuel
     end
 end
--- spec/day1_spec.rb
+++ spec/day1_spec.rb
@@ -4,7 +4,7 @@ describe FuelCounter do
     it "calculates the fuel required to launch Santa's spaceship modules" do
         expect(FuelCounter::get_total(12)).to eq 2
         expect(FuelCounter::get_total(14)).to eq 2
-        expect(FuelCounter::get_total(1969)).to eq 654
-        expect(FuelCounter::get_total(100756)).to eq 33583
+        expect(FuelCounter::get_total(1969)).to eq 966
+        expect(FuelCounter::get_total(100756)).to eq 50346
     end
 end
