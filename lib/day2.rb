c38a1294c7dddd8263744503cac29d68330b3fb1
Mark Dobbins <mjdobbins12@gmail.com>
2020-04-30 21:23:40 +0100

    day 2 complete

--- lib/day2.rb
+++ lib/day2.rb
@@ -1,6 +1,31 @@
-module Intcode
-    def self.run(program)
+class Intcode
+    def run(program)
         seq = program.split(",").map! { |int| int.to_i }
+        seq = process(seq)
+        seq.join(",")
+    end
+
+    def gravity_assist(program, output)
+        seq = program.split(",").map! { |int| int.to_i }
+        i = 0
+        j = 0
+        100.times do
+            100.times do
+                seq_copy = seq.dup
+                seq_copy[1] = i
+                seq_copy[2] = j
+                process(seq_copy)
+                return [i, j] if seq_copy[0] == output
+                j += 1
+            end
+            i += 1
+            j = 0
+        end
+    end
+
+    private
+
+    def process(seq)
         index = 0
         while true do
             case seq[index]
@@ -12,7 +37,7 @@ module Intcode
                 seq[seq[index + 3]] = seq[seq[index + 1]] * seq[seq[index + 2]]
                 index += 4
             when 99
-                return seq.join(",")
+                return seq
             end
         end
     end
